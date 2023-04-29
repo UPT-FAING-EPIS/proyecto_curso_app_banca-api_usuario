93d14b2e7c75544773dfd096c026a7b93c5886e4
diegog <diegorbenom@upt.pe>
2023-04-29 15:38:12 -0500

    Agrega la funcionalidad de Registro de usuarios Agrega los endpoints de registro de usuarios Agrega Dockerfile y .dockerignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(
-      {
-        type: 'mysql',
-        host: 'api-usuario-db-ins.c5odpn9hksgv.us-east-1.rds.amazonaws.com',
-        username: 'su',
-        ssl: false,
-        password: 'upt2023-API',
-        database: 'Api_Usuario',
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true
-      }),
-    UsersModule],
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'api-usuario-db-ins.c5odpn9hksgv.us-east-1.rds.amazonaws.com',
+      port: 3306,
+      username: 'su',
+      ssl: false,
+      password: 'upt2023-API',
+      database: 'Api_Usuario',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
