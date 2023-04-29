2f3f59b1568e147989baa3341f77ccd16dca27af
Abraham Lipa Calabilla <abrlipac@upt.pe>
2023-04-29 11:44:35 -0500

    Agrega el soporte para MySQL con TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(
+      {
+        type: 'mysql',
+        host: 'api-usuario-db-ins.c5odpn9hksgv.us-east-1.rds.amazonaws.com',
+        username: 'su',
+        ssl: true,
+        password: 'upt2023-API',
+        database: 'api_usuario',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true
+      }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
